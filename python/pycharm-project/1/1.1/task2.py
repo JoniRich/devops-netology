097b8de007f2186756774ea332ebc68c93e360d4
Joni <apostol161077@gmail.com>
2021-10-20 13:08:58 +0300

    added python & scripts folder for python-netology project

---
+++ python/pycharm-project/1/1.1/task2.py
@@ -0,0 +1,13 @@
+side_square = int(input('Введите размер стороны квадрата:'))
+perimeter_square = 4 * side_square
+area_square = side_square ** 2
+print('Периметр', perimeter_square)
+print('Площадь', area_square)
+
+length_rectangle = int(input('Введите длину прямоугольника:'))
+width_rectangle = int(input('Введите ширину прямоугольника:'))
+
+perimeter_rectangle = 2 * (length_rectangle + width_rectangle)
+area_rectangle = length_rectangle * width_rectangle
+print('Периметр', perimeter_rectangle)
+print('Площадь', area_rectangle)
