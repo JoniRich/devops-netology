2671cd7303cac08f02521f584033f936d925e62d
Joni <apostol161077@gmail.com>
2021-10-23 19:33:05 +0300

    create & edition file to task 1.2.1 task1_hackerrang.py of devops-python project

---
+++ python/pycharm-project/1/1.2/task1_hackerrang.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# -*-coding: utf-8-*-
+# Задача выполнить необходмо в HackerRang ссылка на задачу https://www.hackerrank.com/challenges/py-if-else/problem
+# Учитывая целое число,выполните следующие условные действия:
+# Если  нечетное, напечатайте Weird
+# Если  является четным и находится в инклюзивном диапазоне 2  к 5 , Распечатать Not Weird
+# Если  является четным и находится в инклюзивном диапазоне 6 к , 20 Распечатать Weird
+# Если  даже и больше, чем 20 , Распечатать Not Weird
+
+# n = int(input("Введите целое число :")) и так тоже работает а на сайте только вот так: n = int(input().strip())
+n = int(input("Введите число: ").strip())
+
+if 1 <= n <= 100:
+    result = n % 2
+    if result != 0:
+        print("Weird")
+    elif 5 >= n >= 2:
+        print("Not Weird")
+    elif 6 <= n <= 20:
+        print("Weird")
+    elif 20 <= n <= 100:
+        print("Not Weird")
+exit(0)
